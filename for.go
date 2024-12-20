31718a4fd9c4d2f988e40ab19f4ebeb3aa10d5a8
JuggerPika <vanben2005@gmail.com>
2024-12-20 17:33:28 +0200

    add new files

---
+++ for.go
@@ -0,0 +1,36 @@
+package main
+
+// import "fmt"
+
+// func main(){
+// 	// first methonds
+// 	// i := 1
+// 	// for i <= 3 {
+// 		// fmt.Println(i)
+// 		// i++
+// 	// }
+
+// 	// second methods
+// 	for j := 0; j < 3; j++ {
+// 		fmt.Println(j)
+// 	}
+
+// 	// third methods
+// 	for i := range 3 {
+// 		fmt.Println("range", i)
+// 	}
+
+// 	// forth methods
+// 	for {
+// 		fmt.Println("loop")
+// 		break
+// 	}
+
+// 	// fifth
+// 	for n := range 10 {
+// 		if n%2 == 0 {
+// 			fmt.Println(n)
+// 		}
+// 		continue
+// 	}
+// }
